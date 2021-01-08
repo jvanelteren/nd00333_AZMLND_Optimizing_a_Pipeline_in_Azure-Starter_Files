742a22fbfef860429d7d2946ba88ad9353f45c56
Jesse van Elteren <jvanelteren@gmail.com>
2021-01-08 20:32:57 +0100

    pushed into repo

--- train.py
+++ train.py
@@ -1,3 +1,4 @@
+#%%
 from sklearn.linear_model import LogisticRegression
 import argparse
 import os
@@ -10,19 +11,9 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
+# Create TabularDataset using TabularDatasetFactory
+data_url = ["https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"]
+ds = TabularDatasetFactory.from_delimited_files(data_url)
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,7 +40,17 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
+    return x_df, y_df
     
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=112)
+
+run = Run.get_context()
+
+
 
 def main():
     # Add arguments to script
@@ -66,7 +67,10 @@ def main():
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
     accuracy = model.score(x_test, y_test)
+    os.makedirs('outputs', exist_ok=True)
+    joblib.dump(model, 'outputs/model.joblib')
     run.log("Accuracy", np.float(accuracy))
 
 if __name__ == '__main__':
     main()
+# %%
